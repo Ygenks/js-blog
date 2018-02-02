3079445de7cb1ad290ae4cf134e60e7ba2315226
Yauheni Saldatsenka <yauheni_saldatsenka@epam.com>
2018-02-02 15:54:37 +0300

    Basic feed

---
+++ feed.js
@@ -0,0 +1,44 @@
+/*jshint esversion: 6 */
+
+function createEntry(article) {
+
+  var entry = document.createElement("div");
+  entry.setAttribute("id", "entry");
+
+
+  var header = document.createElement("h3");
+  var link = document.createElement("a");
+  link.href = article.url;
+  link.textContent = article.title;
+
+  var image = document.createElement("img");
+  image.src = article.urlToImage;
+  image.alt = article.title;
+  image.width = 72;
+  image.height = 72;
+
+  var description = document.createTextNode(article.description);
+
+
+  header.appendChild(link);
+  entry.appendChild(header);
+  entry.appendChild(image);
+  entry.appendChild(description);
+
+  document.body.appendChild(entry);
+}
+
+var url = 'https://newsapi.org/v2/top-headlines?' +
+            'country=us&' +
+            'apiKey=c292fa2e031e40d6b871c85a2fc8d258';
+
+var request = new Request(url);
+
+fetch(request)
+.then((response) => response.json())
+.then(function(feed) {
+
+  for(var article of feed.articles) {
+  	createEntry(article);
+  }
+});
