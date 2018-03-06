0f6270cdd1e3fef9c558401553ce014877ed1b8f
Yauheni Saldatsenka <yauheni_saldatsenka@epam.com>
2018-03-06 13:36:04 +0300

    Add structural and creational patterns

--- src/index.js
+++ src/index.js
@@ -1,26 +1,52 @@
 import '../pages.css'
 
-var button = document.createElement("button");
-button.textContent = "Show News";
-document.body.appendChild(button);
+const url = 'https://newsapi.org/v2/top-headlines?' +
+      'country=us&' +
+      'apiKey=c292fa2e031e40d6b871c85a2fc8d258';
 
-button.addEventListener("click", function() {
 
-    import('./lib/markup').then(markup => {
-        if (process.env.NODE_ENV !== 'production') {
-            console.log('Development mode');
-        }
+var Feed = function(url) {
+    this.url = url;
+}
 
-        var url = 'https://newsapi.org/v2/top-headlines?' +
-            'country=us&' +
-            'apiKey=c292fa2e031e40d6b871c85a2fc8d258';
+Feed.prototype = {
 
-        var request = new Request(url);
+    addShowButton: function() {
+        this.button = document.createElement("button");
+
+        this.button.textContent = "Show News";
+        document.body.appendChild(this.button);
+
+        this.addButtonEvent();
+
+    },
+
+    addButtonEvent: function() {
+        var that = this;
+        this.button.addEventListener("click", function() {
+            import('./lib/markup').then(markup => {
+                if (process.env.NODE_ENV !== 'production') {
+                    console.log('Development mode');
+                }
+                that.fetchFeed(markup);
+            });
+        });
+    },
+
+    fetchFeed: function(markup) {
+        var request = new Request(this.url);
 
         fetch(request)
             .then((response) => response.json())
             .then(function(feed) {
-                feed.articles.filter((article) => article.description != null).map(markup.createEntry);
+                feed.articles.filter((article) => article.description != null).map(markup.createFeedEntry);
             });
-    });
-});
+    }
+}
+
+function main() {
+    var feed = new Feed(url);
+    feed.addShowButton();
+}
+
+main();
--- src/lib/markup.js
+++ src/lib/markup.js
@@ -1,22 +1,14 @@
-export { createEntry };
+export { createFeedEntry };
 
-function createEntry(article) {
+function createFeedEntry(article) {
 
-    var entry = document.createElement("figure");
-    entry.className = "entry";
+    var factory = new ElementFactory(article);
 
-    var header = document.createElement("h3");
-    var link = document.createElement("a");
-    link.href = article.url;
-    link.textContent = article.title;
-
-    var image = document.createElement("img");
-    image.src = article.urlToImage;
-    image.alt = article.title;
-
-    var description = document.createElement("figcaption");
-    description.className = "caption";
-    description.textContent = article.description;
+    var entry = factory.createElement("entry")
+    var header = factory.createElement("header");
+    var link = factory.createElement("link");
+    var image = factory.createElement("image");
+    var description = factory.createElement("description");
 
     header.appendChild(link);
     entry.appendChild(header);
@@ -24,4 +16,65 @@ function createEntry(article) {
     entry.appendChild(description);
 
     document.body.appendChild(entry);
+}
+
+function ElementFactory(article) {
+    this.createElement = function(type) {
+        var element;
+
+        if(type == "entry") {
+            element = new Entry(article);
+        } else if (type == "header") {
+            element = new Header(article);
+        } else if (type == "link") {
+            element = new Link(article);
+        } else if (type == "image") {
+            element = new Image(article);
+        } else if (type == "description") {
+            element = new Description(article);
+        }
+
+        return element;
+    }
+}
+
+var Entry = function (article) {
+    var element = document.createElement("figure");
+
+    element.className = "entry";
+
+    return element;
+}
+
+var Header = function (article) {
+    var element = document.createElement("h3");
+
+    return element;
+}
+
+var Link = function (article) {
+    var element = document.createElement("a");
+
+    element.href = article.url;
+    element.textContent = article.title;
+
+    return element;
+}
+
+var Image = function(article) {
+    var element = document.createElement("img");
+
+    element.src = article.urlToImage;
+    element.alt = article.title;
+
+    return element;
+}
+
+var Description = function(article) {
+    var element = document.createElement("figcaption");
+
+    element.className = "caption";
+    element.textContent = article.description;
+
+    return element;
 }
